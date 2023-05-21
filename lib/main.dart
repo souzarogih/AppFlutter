b203e0ab668cd709152e15f2f5861ace67d9cf3b
HigorSouza <higor.andrade@hotmail.com>
2023-05-21 06:58:47 -0300

    Primeiro commit com flutter

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(AppWidget(
+    title: 'Flutterando Pay',
+  ));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 50.0),
+        ),
+      ),
+    );
+  }
+}
