046f441bef11ef1e4c699c2c9b17182c9b90a99b
HigorSouza <higor.andrade@hotmail.com>
2023-06-25 11:53:21 -0300

    Melhorias na tela do login

--- README.md
+++ README.md
@@ -24,3 +24,12 @@ https://www.youtube.com/watch?v=dgg2zPc5jqM
 Passo 2 - Para ativar o modo desenvolvedor Android, você deve clicar sete vezes na opção “Número da versão”, “Versão da MIUI” ou “Número de compilação” (dependendo do celular) até que a mensagem “agora você é um desenvolvedor" seja exibida. Dê um intervalo de no máximo um segundo entre um toque e outro.
 
 Executar: F5
+
+
+
+Comando para instalar todas as dependências do aplicativo:
+
+https://www.devmedia.com.br/exemplo/planetario-aplicativo-flutter-com-listas-imagens-e-mais/100#:~:text=Para%20clonar%20o%20reposit%C3%B3rio%20deste,e%20prossiga%20com%20a%20instala%C3%A7%C3%A3o.
+
+
+flutter packages get
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,60 +12,96 @@ class _LoginPAgeState extends State<LoginPage> {
   String email = '';
   String password = '';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 200,
-                  height: 200,
-                  child: Image.asset('assets/imagens/elogo.png'),
-                  // child: Image.network(
-                  // 'https://static.wixstatic.com/media/0784b1_18c5cd77477745979c1555f37db576df~mv2.png/v1/fill/w_270,h_262,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/All_Logos_01.png'),
-                ),
-                Container(height: 30),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      labelText: 'Email', border: OutlineInputBorder()),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: 'Password', border: OutlineInputBorder()),
-                ),
-                SizedBox(height: 15),
-                ElevatedButton(
-                  onPressed: () {
-                    if (email == 'higorsand@gmail.com' && password == '123') {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print('Login inválido');
-                    }
-                  },
-                  child: Text(
-                      'Entrar'), //Futuramente incluir aqui o envio de log para uma api
-                )
-              ],
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 200,
+                    height: 200,
+                    child: Image.asset('assets/imagens/elogo.png'),
+                    // child: Image.network(
+                    // 'https://static.wixstatic.com/media/0784b1_18c5cd77477745979c1555f37db576df~mv2.png/v1/fill/w_270,h_262,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/All_Logos_01.png'),
+                  ),
+                  Container(height: 30),
+                  Card(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 12, right: 12, top: 20, bottom: 12),
+                      child: Column(children: [
+                        TextField(
+                          onChanged: (text) {
+                            email = text;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                              labelText: 'Email', border: OutlineInputBorder()),
+                        ),
+                        SizedBox(height: 10),
+                        TextField(
+                          onChanged: (text) {
+                            password = text;
+                          },
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              labelText: 'Password',
+                              border: OutlineInputBorder()),
+                        ),
+                        SizedBox(height: 15),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (email == 'higorsand@gmail.com' &&
+                                password == '123') {
+                              Navigator.of(context)
+                                  .pushReplacementNamed('/home');
+                            } else {
+                              print('Login inválido');
+                            }
+                          },
+                          child: Container(
+                            width: double.infinity,
+                            child: Text(
+                              'Entrar',
+                              textAlign: TextAlign.center,
+                            ),
+                          ), //Futuramente incluir aqui o envio de log para uma api
+                        )
+                      ]),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      ),
+      ],
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/imagens/background.png',
+              fit: BoxFit.cover,
+            )),
+        Container(
+          color: Colors.black.withOpacity(0.6),
+        ),
+        _body(),
+      ],
+    ));
   }
 }
